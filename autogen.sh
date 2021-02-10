664465fe830247c4fbb7d9f0f852fa7967293726
Andy Shevchenko <andriy.shevchenko@linux.intel.com>
2021-02-10 20:41:33 +0200

    autofoo: Drop silly version check for automake and autoconf
    New distributions should already move to the latest and greatest ones.
    
    Signed-off-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>

--- autogen.sh
+++ autogen.sh
@@ -6,8 +6,6 @@ test "$PKG_NAME" || PKG_NAME=Package
 test "$srcdir" || srcdir=.
 
 # default version requirements ...
-test "$REQUIRED_AUTOCONF_VERSION" || REQUIRED_AUTOCONF_VERSION=2.53
-test "$REQUIRED_AUTOMAKE_VERSION" || REQUIRED_AUTOMAKE_VERSION=1.9
 test "$REQUIRED_LIBTOOL_VERSION" || REQUIRED_LIBTOOL_VERSION=1.5
 test "$REQUIRED_GETTEXT_VERSION" || REQUIRED_GETTEXT_VERSION=0.12
 test "$REQUIRED_GLIB_GETTEXT_VERSION" || REQUIRED_GLIB_GETTEXT_VERSION=2.2.0
@@ -302,27 +300,10 @@ for configure_ac in $configure_files; do
     fi
 done
 
-#tell Mandrake autoconf wrapper we want autoconf 2.5x, not 2.13
-WANT_AUTOCONF_2_5=1
-export WANT_AUTOCONF_2_5
-version_check autoconf AUTOCONF 'autoconf2.50 autoconf autoconf-2.53' $REQUIRED_AUTOCONF_VERSION \
-    "http://ftp.gnu.org/pub/gnu/autoconf/autoconf-$REQUIRED_AUTOCONF_VERSION.tar.gz"
-AUTOHEADER=`echo $AUTOCONF | sed s/autoconf/autoheader/`
-
-case $REQUIRED_AUTOMAKE_VERSION in
-    1.4*) automake_progs="automake-1.4" ;;
-    1.5*) automake_progs="automake-1.12 automake-1.11 automake-1.10 automake-1.9 automake-1.8 automake-1.7 automake-1.6 automake-1.5" ;;
-    1.6*) automake_progs="automake-1.12 automake-1.11 automake-1.10 automake-1.9 automake-1.8 automake-1.7 automake-1.6" ;;
-    1.7*) automake_progs="automake-1.12 automake-1.11 automake-1.10 automake-1.9 automake-1.8 automake-1.7" ;;
-    1.8*) automake_progs="automake-1.12 automake-1.11 automake-1.10 automake-1.9 automake-1.8" ;;
-    1.9*) automake_progs="automake-1.12 automake-1.11 automake-1.10 automake-1.9" ;;
-    1.10*) automake_progs="automake-1.12 automake-1.11 automake-1.10" ;;
-    1.11*) automake_progs="automake-1.12 automake-1.11" ;;
-    1.12*) automake_progs="automake-1.12" ;;
-esac
-version_check automake AUTOMAKE "$automake_progs" $REQUIRED_AUTOMAKE_VERSION \
-    "http://ftp.gnu.org/pub/gnu/automake/automake-$REQUIRED_AUTOMAKE_VERSION.tar.gz"
-ACLOCAL=`echo $AUTOMAKE | sed s/automake/aclocal/`
+AUTOCONF='autoconf'
+AUTOHEADER="autoheader"
+AUTOMAKE="automake"
+ACLOCAL="aclocal"
 
 if $want_libtool; then
     version_check libtool LIBTOOLIZE libtoolize $REQUIRED_LIBTOOL_VERSION \
@@ -469,11 +450,7 @@ for configure_ac in $configure_files; do
         if [ -f INSTALL ]; then
           cp -pf INSTALL INSTALL.autogen_bak
         fi
-	if [ $REQUIRED_AUTOMAKE_VERSION != 1.4 ]; then
-	    $AUTOMAKE --foreign --add-missing --force --copy || exit 1
-	else
-	    $AUTOMAKE --foreign --add-missing --copy || exit 1
-	fi
+	$AUTOMAKE --foreign --add-missing --force --copy || exit 1
         if [ -f COPYING.autogen_bak ]; then
           cmp COPYING COPYING.autogen_bak > /dev/null || cp -pf COPYING.autogen_bak COPYING
           rm -f COPYING.autogen_bak
